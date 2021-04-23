4480e8df54a2ed357f684efe693af4c60f519c48
Mukdo <bmukdo@gmail.com>
2021-04-23 20:55:00 +0600

    push library

---
+++ BitcoinPrice.Library/BitCoinPrice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinPrice.Library
+{
+    public class BitCoinPrice : Entity
+    {
+        public virtual Time time { get; set; }
+        public string disclaimer { get; set; }
+        public string chartName { get; set; }
+        public virtual Bpi bpi { get; set; }
+    }
+}
---
+++ BitcoinPrice.Library/Bpi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitcoinPrice.Library
+{
+    public class Bpi : Entity
+    {
+        public  USD USD { get; set; }
+        public  GBP GBP { get; set; }
+        public  EUR EUR { get; set; }
+    }
+}
